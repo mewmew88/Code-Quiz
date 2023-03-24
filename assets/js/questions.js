19dc3408d1330c006c6fab96da85136deae74f47
mewmew88 <mehr_hejazi@hotmail.com>
2023-03-24 20:07:58 +0000

    ammended errors

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,19 +9,19 @@ var questions = [
     title: "Which movie kicked off the Marvel Cinematic Universe?",
     choice: ["Captain America", "X-Men", "Iron Man", "The Dark Knight"],
     answer: "Iron Man"
-  }
+  },
 
   {
     title: "Who is the Winter Soldier?",
     choice: ["Howard Stark", "Peggy Carter", "Bruce Banner", "Bucky Barnes"],
     answer: "Bucky Barnes"
-  }
+  },
 
   {
     title: "In which movie did Spider-Man make his first appearance in the MCU?",
     choice: ["Captain America: Civil War","SpiderMan: No Way Home","Captain America: Civil War","SpiderMan: Homecoming"],
     answer: "Captain America: Civil War"
-  }
+  },
 
   {
     title: "What is the name of the villain in the first Thor movie?",
