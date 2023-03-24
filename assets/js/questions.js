39985bd25255ad017cf8bedb022ada08f78c305a
mewmew88 <mehr_hejazi@hotmail.com>
2023-03-24 19:23:02 +0000

    questions added to question.js

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+  {
+    title: "Who is the oldest Avenger?",
+    choice: ["Iron Man", "Winter Solider", "Captain America", "Thor"],
+    answer: "Thor"
+  },
+
+  {
+    title: "Which movie kicked off the Marvel Cinematic Universe?",
+    choice: ["Captain America", "X-Men", "Iron Man", "The Dark Knight"],
+    answer: "Iron Man"
+  }
+
+  {
+    title: "Who is the Winter Soldier?",
+    choice: ["Howard Stark", "Peggy Carter", "Bruce Banner", "Bucky Barnes"],
+    answer: "Bucky Barnes"
+  }
+
+  {
+    title: "In which movie did Spider-Man make his first appearance in the MCU?",
+    choice: ["Captain America: Civil War","SpiderMan: No Way Home","Captain America: Civil War","SpiderMan: Homecoming"],
+    answer: "Captain America: Civil War"
+  }
+
+  {
+    title: "What is the name of the villain in the first Thor movie?",
+    choice: ["Loki", "Thanos", "Hela", "Ultron"],
+    answer: "Hela"
+  }
+]
